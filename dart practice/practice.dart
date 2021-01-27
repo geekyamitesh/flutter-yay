460cd2269bd30bdb3ab29d123475a36af9e1cadb
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2021-01-27 16:14:47 +0530

    first commit

---
+++ dart practice/practice.dart
@@ -0,0 +1,78 @@
+// Dart is object oriented
+// double sum(int n1, double n2) {
+//   return n1 + n2;
+// }
+
+// void main() {
+// double s;
+// //This function must be write in your particular dart code.
+// s = sum(3, 4.5);
+// print(s);
+
+// Datat types:
+/*
+ 1. Numbers 
+ 2. String
+ 3. Booleans
+ 4. Lists :- It is collection of data (list is an ordered group of data)
+ 5. Maps <- It is collection of data (It represnts with key values pair)
+  
+
+  dynamic
+  */
+
+//
+// it will through error because we have been trying to use diffferent datatypes.
+// var s = 10;
+// s ='amitesh';
+//   // const s = 10;
+
+//   var s = 10;
+//   s = 11;
+//   print(s);
+
+// //lis[index] = value;
+
+//   var list = new List();
+
+// list[0] = 11;
+// list[1] = 'amitesh';
+// list[2] = 12.5;
+// list.add(13);
+// list.add(45);
+// print(list);
+
+//Maps:- The map contain collection of data but it is diffrent than list :- Because map object is a simple key/value pair;
+// //  var mp = {key1:value, key2=value2.......}
+
+//   var details = {'name': 'sneha', 'class': 12};
+
+//   print(details);
+// }
+
+// i will use for single loine comment
+
+/*
+multile line line comment
+*/
+
+//excute your code through terminal also
+
+// CLASS
+
+class Studnets {
+  // this particular function is called method
+  void class_name() {
+    print("my class name is 12-c");
+  }
+
+  String s1 = 'Sneha patil';
+  int marks = 70;
+}
+
+void main() {
+  Studnets s2 = new Studnets();
+
+  s2.class_name();
+  print('my name is ');
+}
---
+++ dart practice/tempCodeRunnerFile.dart
@@ -0,0 +1,9 @@
+// const s = 10;
+
+//   var s = 10;
+//   s = 11;
+//   print(s);
+
+// //lis[index] = value;
+
+//   var list = new List();
