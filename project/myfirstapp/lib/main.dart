84c4bdb8a34ccf844250991d192ae6f87919e28e
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2021-02-01 20:00:52 +0530

    added some widget

--- project/myfirstapp/lib/main.dart
+++ project/myfirstapp/lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+/*
+MaterialApp
+Scaffold
+AppBar
+TabBar
+TabBarView
+ListTitle
+.
+.
+.
+LayoutWidget :- {
+  1. Container
+  2. Center
+  3. Row
+  4. Column
+  5. Stack
+}
+State maintenance widget :- 1. stateless & stateful
 
+*/
+
+void main() {
+  runApp(MyApp());
+}
+
+// with help of extends keyboard we are inherited stalesswidget
 class MyApp extends StatelessWidget {
   @override
+
+  //you can memorized this line
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Flutter GS Yay class'),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter  Class'),
+      ),
+      body: Container(
+        alignment: Alignment.centerLeft,
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () => {},
+      ),
+    ));
   }
 }
